08ce593ee2d08487b80209fcd8d5702c85b36217
muhammdfayyazansari <fayyazansari268@gmail.com>
2022-09-21 18:29:17 +0500

    express server

--- server.mjs
+++ server.mjs
@@ -1,9 +1,9 @@
 import express from 'express'
 const app = express()
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World abdullah!')
+  res.send('Hello World from Abdullah Nomani!')
   
 })
 
