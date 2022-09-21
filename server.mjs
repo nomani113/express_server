7a116e3cd7bc1785f379fb3ca6da977aabde9b3b
muhammdfayyazansari <fayyazansari268@gmail.com>
2022-09-21 18:16:59 +0500

    server.express

---
+++ server.mjs
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World abdullah!')
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
